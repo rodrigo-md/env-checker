3f64972e2b2fb58090a0b571ba265523bde10653
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-10-07 02:56:11 -0300

    fix(webpack): add backward compatibility to node12 when using fs/promises package
    use string-replace-webpack-plugin to replace the line of code that import fs/promises for importing
    promises from "fs" and then destructuring the promises object

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const ShebangPlugin = require("webpack-shebang-plugin");
+const StringReplacePlugin = require("string-replace-webpack-plugin");
 
 module.exports = (async () => {
   // https://github.com/swc-project/swc-loader/issues/56
@@ -17,7 +18,7 @@ module.exports = (async () => {
       },
     },
     target: "node",
-    mode: "production",
+    mode: "none",
     module: {
       rules: [
         {
@@ -30,13 +31,30 @@ module.exports = (async () => {
             },
           },
         },
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: {
+            loader: StringReplacePlugin.replace({
+              replacements: [
+                {
+                  pattern: /import {(?<promisesMethods>.+)} from (?<quotes>"|')node:fs\/promises\2(?<eol>;?\n)?/g,
+                  replacement: function(match, p1,p2,p3, offset, string, groups) {
+                    const { promisesMethods, quotes, eol } = groups;
+                      return `import { promises as fspromises } from ${quotes}node:fs${quotes}${eol}const {${promisesMethods}} = fspromises;\n`;
+                  }
+                }
+              ]
+            }),
+          },
+        },
       ],
     },
     resolve: {
       extensions: [".ts", ".js"],
     },
     externals: [
-      // rename node:<module> => module for backward compatibility with versions < node16
+      // rename node:<module> => module for backward compatibility with versions < node18
       function ({ request }, callback) {
         if (/^node:/.test(request)) {
           return callback(null, `commonjs ${request.replace(/node:/, "")}`);
