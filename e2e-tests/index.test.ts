8034ad616c60c251ea462ae073cbe6aa8b705129
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-10-12 00:41:45 -0300

    test(e2e): add e2e tests and exclude them from npm test

---
+++ e2e-tests/index.test.ts
@@ -0,0 +1,16 @@
+import t from 'tap';
+import path from 'node:path';
+import  { exec } from 'node:child_process';
+import { readFile } from 'node:fs/promises';
+import { promisify } from 'node:util';
+
+
+const execCmd = promisify(exec);
+
+t.test('published version in the distribution channel match the current branch', async(t) => {
+    const pkg = JSON.parse(await readFile(path.resolve(__dirname,'../package.json'), {encoding: 'utf8'}));
+
+    const { stdout } = await execCmd('envchecker --version');
+
+    t.match(stdout, new RegExp(`${pkg.version}`, 'gi'));
+}); 
