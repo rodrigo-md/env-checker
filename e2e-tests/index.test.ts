85d82ef870843d164bef7f0b1e438456d3c1bf8c
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-10-13 20:03:37 -0300

    test(e2e): read from environment the target node version to run envchecker cli

--- e2e-tests/index.test.ts
+++ e2e-tests/index.test.ts
@@ -10,7 +10,7 @@ const execCmd = promisify(exec);
 t.test('published version in the distribution channel match the current branch', async(t) => {
     const pkg = JSON.parse(await readFile(path.resolve(__dirname,'../package.json'), {encoding: 'utf8'}));
 
-    const { stdout } = await execCmd('envchecker --version');
+    const { stdout } = await execCmd(`$HOME/.nvm/versions/node/v${process.env.TARGET_NODE}/bin/node /usr/local/bin/envchecker --version`);
 
     t.match(stdout, new RegExp(`${pkg.version}`, 'gi'));
 }); 
