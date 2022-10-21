70135e937591d3b382eae7b70dce4c829fd4ff7c
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2022-10-21 11:35:21 -0300

    test(e2e): stop reading version from package.json and now receive it as an environment variable
    updating the package.json with the version to be read on tests was causing conflict when multiple
    branch (like prereleases) update the version

--- e2e-tests/index.test.ts
+++ e2e-tests/index.test.ts
@@ -1,21 +1,14 @@
 import t from "tap";
-import path from "node:path";
-import { readFile } from "node:fs/promises";
 import { execCmd } from "./test-utils/exec-cmd";
 
 t.test(
 	"published version in the distribution channel match the current branch",
 	async (t) => {
-		const pkg = JSON.parse(
-			await readFile(path.resolve(__dirname, "../package.json"), {
-				encoding: "utf8",
-			}),
-		);
-
+		const expectedVersion = process.env.VERSION;
 		const { stdout, stderr } = await execCmd("envchecker --version");
 
 		t.ok(stderr.length === 0, `stderr: ${stderr}`);
-		t.match(stdout, new RegExp(`${pkg.version}`, "gi"));
+		t.match(stdout, new RegExp(`${expectedVersion}`, "gi"));
 	},
 );
 
